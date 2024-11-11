cef2ca0491d278bb2fa1302c0111c55065d264b2
romain.fontugne <romain.fontugne@gmail.com>
2024-11-11 15:17:34 +0900

    added link to original repo in the readme

---
+++ cruxdownloader/__main__.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+
+from .downloader import CrUXRepoManager
+
+
+def main(directory, credentials_path):
+    mgr = CrUXRepoManager(directory)
+    credentials_env = None if credentials_path else True
+    mgr.download(credentials_path, credentials_env=credentials_env)
+#    mgr.update_current("current.csv.gz")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='cruxdownloader',
+        description='Download CrUX top lists aggregated by month',
+    )
+    parser.add_argument('directory')
+    parser.add_argument('--credential-file', default=None)
+    args = parser.parse_args()
+    main(args.directory or os.getcwd(), args.credential_file)
---
+++ cruxdownloader/downloader.py
@@ -0,0 +1,149 @@
+from iso3166 import countries
+import json
+import os
+from dateutil import rrule
+import dateutil.relativedelta as relativedelta
+import datetime
+import gzip
+import shutil
+
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+
+class CrUXDownloader:
+
+    GLOBAL_SQL = """SELECT distinct origin, experimental.popularity.rank
+        FROM `chrome-ux-report.experimental.global`
+        WHERE yyyymm = @date AND experimental.popularity.rank <= 1000000
+        GROUP BY origin, experimental.popularity.rank
+        ORDER BY experimental.popularity.rank;"""
+
+    COUNTRY_SQL = """SELECT distinct origin, experimental.popularity.rank
+        FROM `chrome-ux-report.experimental.country`
+        WHERE yyyymm = @date AND experimental.popularity.rank <= 1000000
+        AND country_code = @country_code
+        GROUP BY origin, experimental.popularity.rank
+        ORDER BY experimental.popularity.rank;"""
+
+    def __init__(self, credentials_path=None, credentials_json=None, credentials_env=False):
+        if not credentials_path and not credentials_json and not credentials_env:
+            raise Exception("No credentials supplied for Google Cloud")
+        if credentials_env:
+            self._bq_client = bigquery.Client()
+        elif credentials_json:
+            obj = json.loads(credentials_json)
+            credentials = service_account.Credentials.from_service_account_info(obj)
+            self._bq_client = bigquery.Client(credentials=credentials)
+        else:
+            self._bq_client = bigquery.Client.from_service_account_json(credentials_path)
+
+    def dump_month_to_csv(self, scope, yyyymm: int, path):
+        assert scope == "global" or scope in countries
+        query = None
+        job_config = None
+
+        if scope == "global":
+            query = self.GLOBAL_SQL
+            job_config = bigquery.QueryJobConfig(
+                query_parameters=[
+                    bigquery.ScalarQueryParameter("date", "INT64", yyyymm),
+                ]
+            )
+        else:
+            query = self.COUNTRY_SQL
+            job_config = bigquery.QueryJobConfig(
+                query_parameters=[
+                    bigquery.ScalarQueryParameter("date", "INT64", yyyymm),
+                    bigquery.ScalarQueryParameter("country_code", "STRING", scope),
+                ]
+            )
+
+        df = self._bq_client.query(query, job_config=job_config).to_dataframe()
+        if df.empty:
+            return
+        df.to_csv(path, index=False, header=True)
+        return path
+
+
+class CrUXRepoManager:
+
+    MIN_YYYYMM = datetime.datetime(2024, 9, 1)
+    GLOBAL_DIR_NAME = "global"
+    COUNTRY_DIR_NAME = "country"
+
+    @classmethod
+    def _iter_valid_YYYYMM(cls):
+        now = datetime.datetime.now()
+        last_month = now + relativedelta.relativedelta(months=-1)
+        for dt in rrule.rrule(rrule.MONTHLY, dtstart=cls.MIN_YYYYMM,
+                until=last_month):
+            yield (dt.year, dt.month)
+
+    def __init__(self, data_directory):
+        self._data_directory = data_directory
+        self._global_directory = os.path.join(data_directory,
+                self.GLOBAL_DIR_NAME)
+        self._country_directory = os.path.join(data_directory,
+                self.COUNTRY_DIR_NAME)
+
+    def _get_existing_YYYYMM(self, path):
+        for f in os.listdir(path):
+            if not f.startswith("2"):
+                continue
+            yield int(f[0:4]), int(f[4:6])
+
+    def _to_fetch_YYYYMM(self, path):
+        valid = set(self._iter_valid_YYYYMM())
+        existing = set(self._get_existing_YYYYMM(path))
+        for dt in valid - existing:
+            mm = str(dt[1])
+            if len(mm) == 1:
+                mm = "0" + mm
+            yield int(str(dt[0]) + mm)
+
+    def _make_directories(self):
+        if not os.path.exists(self._global_directory):
+            os.mkdir(self._global_directory)
+        if not os.path.exists(self._country_directory):
+            os.mkdir(self._country_directory)
+        for country in countries:
+            dir = os.path.join(self._country_directory, country.alpha2.lower())
+            if not os.path.exists(dir):
+                os.mkdir(dir)
+
+    def _gzip(self, filename, delete_original=True):
+        with open(filename, 'rb') as f_in:
+            gzipped_filename = filename + ".gz"
+            with gzip.open(gzipped_filename, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        if delete_original:
+           os.remove(filename)
+
+    def download(self, credentials_path=None, credentials_json=None, credentials_env=False):
+        downloader = CrUXDownloader(
+            credentials_path=credentials_path,
+            credentials_json=credentials_json,
+            credentials_env=credentials_env,
+        )
+        self._make_directories()
+        # for scope in {"global", "country"}:
+        for country in countries:
+            scope = country.alpha2.lower()
+            data_directory = self._global_directory if scope == "global" else os.path.join(self._country_directory, scope)
+            for yyyymm in self._to_fetch_YYYYMM(data_directory):
+                print("Fetching {} {}".format(scope, yyyymm))
+                filename = str(yyyymm) + ".csv"
+                results_path = os.path.join(data_directory, filename)
+                if downloader.dump_month_to_csv(scope, yyyymm, results_path):
+                    self._gzip(results_path)
+
+    def update_current(self, dest):
+        # Global Only right now
+        latest = max(self._get_existing_YYYYMM(self._global_directory))
+        latest_filename = str(latest[0]) + str(latest[1]).zfill(2) + ".csv.gz"
+        src_path = os.path.join(self._global_directory, latest_filename)
+        assert(os.path.exists(src_path))
+        dst_path = os.path.join(self._global_directory, dest)
+        shutil.copyfile(src_path, dst_path)
+
